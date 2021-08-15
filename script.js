14250e55a868936f4d39710ed40ddaa39b9df591
thrw-wy-blancc <88911683+thrw-wy-blancc@users.noreply.github.com>
2021-08-15 20:39:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const search = document.querySelector(".search");
+const searchButton = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+searchButton.addEventListener("click", () => {
+    search.classList.toggle("active");
+    input.focus();
+    console.log("me");
+})
